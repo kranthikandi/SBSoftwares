d9775ab8527d6d193a8b09b8374c8365f7d38303
Mallisanneboyina <Mallisanneboyina@gmail.com>
2017-09-17 09:55:38 -0700

    Final Updates

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -1,3 +1,4 @@
+
 $("#calculateBtn").click(function(){
     
     var material = Number($("#material").val());
@@ -12,6 +13,10 @@ $("#calculateBtn").click(function(){
     var final = total+percent;
     $("#total").val(final);
 });
+
+$("#spanGo").click(function(){
+	$("#map").show();
+})
 
 $("#estimate").click(function(){
 	$("#estimateDiv").show();
@@ -108,24 +113,45 @@ window.onload = function() {
 	      icon: L.mapquest.icons.marker(),
 	      draggable: false
 	    }).bindPopup('Denver, CO').addTo(map);
-  L.circle([37.4419 , -122.1430 ], { radius: 9000 }).addTo(map);
+  L.circle([37.3688 , -122.0363 ], { radius: 9000 }).addTo(map);
 
   var scheduleMap = L.mapquest.map('scheduleMap', {
 	    center: [37.4419 , -122.14304 ],
 	    layers: L.mapquest.tileLayer('map'),
 	    zoom: 11
 	  });
-  scheduleMap.addLayer(L.mapquest.trafficLayer());
+  //scheduleMap.addLayer(L.mapquest.trafficLayer());
   scheduleMap.addLayer(L.mapquest.incidentsLayer());
   scheduleMap.addLayer(L.mapquest.marketsLayer());
-	  L.marker([37.7749 , -122.4194 ], {
-	    icon: L.mapquest.icons.marker(),
-	    draggable: false
-	  }).bindPopup('Denver, CO').addTo(scheduleMap);
-	  
-	  L.marker([37.3688 , -122.0363 ], {
-		    icon: L.mapquest.icons.marker(),
-		    draggable: false
-		  }).bindPopup('Denver, CO').addTo(scheduleMap);
+	 
+  L.mapquest.directions().route({
+      start: 'Cupertino, California',
+      end: 'Cupertino, California',
+      waypoints: [ '2173 Benton St, Santa Clara, CA', '14000 Fruitvale Ave, Saratoga, CA']
+    });
+  
+  L.mapquest.directions().route({
+      start: 'Stanford, California',
+      end: 'Stanford, California',
+      waypoints: [ '725 Sylvan Ave, Mountain View, CA', '1656 McCarthy Blvd, Milpitas, CA']
+    });
+  
+  L.mapquest.directions().route({
+      start: 'Snail, Sunnyvale, CA',
+      end: 'Santa Clara, California',
+      waypoints: [ '830 Auzerais Ave, San Jose, CA', '1235 Wildwood Ave, Sunnyvale, CA']
+    });
+  
+  L.mapquest.directions().route({
+      start: 'North San Jose, San Jose, CA',
+      end: 'Santa Clara, California',
+      waypoints: [ '1235 Wildwood Ave, Sunnyvale, CA', '12800-12836 Concepcion Rd, Los Altos Hills, CA']
+    });
+  
+  L.mapquest.directions().route({
+      start: 'Milpitas, California',
+      end: 'Santa Clara, California',
+      waypoints: [ 'Santa Clara, California', '725 Sylvan Ave, Mountain View, CA']
+    });
 
 };
